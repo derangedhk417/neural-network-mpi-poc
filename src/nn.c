fe77b88ecded0f52aae1925c796e3bc6c0129cc9
Adam Robinson <derangedhk417@gmail.com>
2018-10-26 19:04:56 -0400

    More skeleton code

---
+++ src/nn.c
@@ -0,0 +1,8 @@
+/*
+ * This is a basic implementation of a feed forward neural network in C. The 
+ * library handles only fully connected feed forward networks. It is not 
+ * intended for back-propgoation training. The library allows for the 
+ * dimensions of the neural network as will as the actication function
+ * for each layer to be specified.
+ */
+
